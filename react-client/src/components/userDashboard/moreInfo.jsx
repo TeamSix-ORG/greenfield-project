d39b3620b17a9ac44577e4d2ef7644471e96a4f4
SofianSaleh <sofiane.saleh11@gmail.com>
2020-01-07 15:09:28 +0100

    started working on the attend button

--- react-client/src/components/userDashboard/moreInfo.jsx
+++ react-client/src/components/userDashboard/moreInfo.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Attend from './attend'
 
 class MoreInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      event: []
+      event: [],
+      moreInfo: true,
+        attend: false
     };
   }
 
@@ -17,16 +20,31 @@ class MoreInfo extends Component {
     });
   }
 
+  attendToggler(e) {
+    e.preventDefault();
+    this.setState({
+        moreInfo: false,
+        attend: true
+    })
+  }
+
   render() {
     return (
       <div>
         {
+            this.state.moreInfo ?
           <div>
             <img src={this.state.event[0].img} />
             <h3>{this.state.event[0].title}</h3>
             <h3>{this.state.event[0].date}</h3>
             <p>{this.state.event[0].description}</p>
+            <p>{this.state.event[0].cost}</p>
+            <button type="submit" onClick={this.attendToggler.bind(this)}>
+              Attend
+            </button>
           </div>
+          :
+          <Attend />
         }
       </div>
     );
