2b2ab30151ddad251082de589f41cfe3b6fec1f7
waj949 <wajdihamrouni1993@gmail.com>
2020-01-08 03:15:17 +0100

    server side validation

--- server/index.js
+++ server/index.js
@@ -3,39 +3,66 @@ const bodyParser = require('body-parser');
 const db = require('../database-mongo/index');
 const path = require('path');
 var Mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 const User = require('../database-mongo/users')
+const {signUpValidation,loginValidation} = require('../validation')
+const bcrypt = require("bcryptjs")
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/../react-client/dist'));
+
 
 app.get('*', (req, res) => {
 	res.sendFile('index.html', { root: path.join(__dirname, '../react-client/dist') });
 });
 
 app.post('/signupuser', async (req, res)=> {
-	const user = new User({
+// DATA VALIDATION
+const {error} = signUpValidation({username:req.body.username, email:req.body.email, password: req.body.password})
+if (error) return res.status(400).json(error.details[0].message)
+
+//checking if the email exists
+const emaiExist = await User.findOne({email:req.body.email})
+if(emaiExist) { res.status(400).json("Email already exists")}
+
+//hashing the password 
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+
+//Creating a user 
+const user = new User({
 		_id: new Mongoose.Types.ObjectId(),
 		username:req.body.username,
 		fullname: req.body.name,
 		email: req.body.email,
 		phoneNumber: req.body.phoneNumber,
-		password: req.body.password,
+		password: hashedPassword,
 		type: req.body.type,
 		profileId: "110", 
 	})
 	try {
 		const savedUser = await user.save()
 		console.log(savedUser)
-		res.json(savedUser)
+		res.json(savedUser.username)
 	} catch(err){
 		console.log(err)
 		res.status(400).json(err)
 	}
 });
-app.post('/login',(req,res)=>{
-	console.log(req.body.username)
-	res.json('rrrr')
+app.post('/login', async (req,res)=>{
+	// DATA VALIDATION
+	const {error} = loginValidation({username:req.body.username, email:req.body.email, password: req.body.password})
+	if (error) return res.status(400).json(error.details[0].message)
+	//check email
+	const user = await User.findOne({email:req.body.email})
+	if(!user) return res.status(400).json("Email does not exists")
+
+	const validPass = await bcrypt.compare(req.body.password, user.password)
+	if(!validPass) return res.status(400).json("wrong password")
+
 })
 let port = 3001;
 
