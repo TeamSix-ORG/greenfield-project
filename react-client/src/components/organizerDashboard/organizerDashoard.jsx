571c844025674f87791fe0c89cb56a5e5761a8dc
amineslaimia <medamineslaimia@gmail.com>
2020-01-10 13:17:25 +0100

    package

--- react-client/src/components/organizerDashboard/organizerDashoard.jsx
+++ react-client/src/components/organizerDashboard/organizerDashoard.jsx
@@ -9,13 +9,36 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 class OrganizerDashboard extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			createEvent: false
+		};
+	}
+	toggleStates(e) {
+		e.preventDefault();
+		this.setState({
+			createEvent: true
+		});
 	}
 
 	render() {
+		if (this.state.redirectToAttendedEvents) {
+			this.setState({
+				redirectToAttendedEvents: false
+			});
+			return (
+				<Redirect
+					to={{
+						pathname: '/AttendedEvents'
+					}}
+				/>
+			);
+		}
 		return (
 			<div>
 				<h1>OrganizerDashboard</h1>
+				<button type="submit" onClick={this.toggleStates.bind(this)}>
+					Create new event
+				</button>
 			</div>
 		);
 	}
