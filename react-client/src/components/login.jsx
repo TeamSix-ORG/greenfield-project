a47799e3b3815f0bd5e308bbbad0bbcf6d0078f1
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2020-01-10 15:47:27 +0100

    routing

--- react-client/src/components/login.jsx
+++ react-client/src/components/login.jsx
@@ -1,101 +1,116 @@
-import React from 'react';
-import $ from 'jquery';
-import { Link } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import $ from "jquery";
+import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 class Login extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			username: '',
-			email: '',
-			password: '',
-			type: 'user',
-			authentified: false
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	handleChange(e) {
-		this.setState({ [e.target.name]: e.target.value });
-	}
-	handleSubmit(event) {
-		event.preventDefault();
-		$.ajax({
-			type: 'POST',
-			url: '/api/login',
-			dataType: 'text',
-			data: this.state,
-			contentType: 'application/x-www-form-urlencoded',
-			success: (data) => {
-				console.log(data);
-				console.log(this.state);
-				this.setState({
-					authentified: true
-				});
-				console.log(this.state);
-				localStorage.setItem('user', JSON.stringify(data));
-			},
-			error: (err) => {
-				if (err) {
-					console.log(err);
-				}
-			}
-		});
-	}
-	render() {
-		if (this.state.authentified) {
-			return (
-				<Redirect
-					to={{
-						pathname: '/userdashboard'
-					}}
-				/>
-			);
-		}
-		const { username, email, password } = this.state;
-		return (
-			<div>
-				<h1>create a user account</h1>
-				<form onSubmit={this.handleSubmit}>
-					<label>
-						Username:
-						<input
-							type="text"
-							placeholder="Choose username"
-							name="username"
-							value={username}
-							onChange={this.handleChange}
-							required
-						/>
-					</label>
-					<label>
-						Email:
-						<input
-							type="text"
-							placeholder="enter your email"
-							name="email"
-							value={email}
-							onChange={this.handleChange}
-							required
-						/>
-					</label>
-					<label>
-						Password:
-						<input
-							type="password"
-							placeholder="enter your password"
-							name="password"
-							value={password}
-							onChange={this.handleChange}
-							required
-						/>
-					</label>
-					<button type="submit">Submit</button>
-				</form>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      type: "",
+      authentified: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/api/login",
+      dataType: "text",
+      data: this.state,
+      contentType: "application/x-www-form-urlencoded",
+      success: data => {
+        //  console.log(this.state);
+        this.setState({
+          authentified: true,
+          type: JSON.parse(data).type
+        });
+        console.log(this.state);
+        localStorage.setItem(this.state.type, JSON.stringify(data));
+        if (this.state.type === "user")
+          this.props.history.push("/userdashboard");
+        if (this.state.type === "organizer")
+          this.props.history.push("/organizerdashboard");
+      },
+      error: err => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    });
+  }
+  render() {
+    // if (this.state.authentified) {
+    //   if (this.state.type === "user") {
+    //     return (
+    //       <Redirect
+    //         to={{
+    //           pathname: "/userdashboard"
+    //         }}
+    //       />
+    //     );
+    //   }
+    //   if (this.state.type === "organizer") {
+    //     return (
+    //       <Redirect
+    //         to={{
+    //           pathname: "/organizerdashboard"
+    //         }}
+    //       />
+    //     );
+    //   }
+    // }
+    const { username, email, password } = this.state;
+    return (
+      <div>
+        <h1>create a user account</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              placeholder="Choose username"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="text"
+              placeholder="enter your email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              placeholder="enter your password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- react-client/src/components/userDashboard/moreInfo.jsx
+++ react-client/src/components/userDashboard/moreInfo.jsx
@@ -1,138 +1,126 @@
-import React, { Component } from 'react';
-import Attend from './attend.jsx';
-import EventsList from './eventsList.jsx';
-import $ from 'jquery';
+import React, { Component } from "react";
+import Attend from "./attend.jsx";
+import EventsList from "./eventsList.jsx";
+import $ from "jquery";
 
 class MoreInfo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			event1: [],
-			moreInfo: true,
-			attendMoney: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      event1: [],
+      moreInfo: true,
+      attendMoney: false
+    };
+  }
 
-	attendToggler(e) {
-		// Send request to the backend with the id of the event and user id
-		var obj = {};
-		obj.userId = '5e17a83d39ddb953b5b2dcc1';
-		obj.eventId = this.props.eventDescription[this.props.index].id;
-		console.log(obj, this.props.userId);
-		$.ajax({
-			url: '/api/jointEventUser',
-			type: 'POST',
-			data: obj,
-			success: (data) => {
-				console.log(data);
-				if (data === 'Joined') {
-					this.setState({
-						attend: false,
-						home: true
-					});
-				}
-			},
-			error: (err) => {
-				throw err;
-			}
-		});
+  attendToggler(e) {
+    // Send request to the backend with the id of the event and user id
+    var obj = {};
+    obj.userId = "5e17a83d39ddb953b5b2dcc1";
+    obj.eventId = this.props.eventDescription[this.props.index].id;
+    console.log(obj, this.props.userId);
+    $.ajax({
+      url: "/api/jointEventUser",
+      type: "POST",
+      data: obj,
+      success: data => {
+        console.log(data);
+        if (data === "Joined") {
+          this.setState({
+            attend: false,
+            home: true
+          });
+        }
+      },
+      error: err => {
+        throw err;
+      }
+    });
 
-		this.updateState();
-	}
+    this.updateState();
+  }
 
-	updateState(e) {
-		// e.preventDefault();
-		this.setState({
-			moreInfo: false,
-			home: true
-		});
-	}
+  updateState(e) {
+    // e.preventDefault();
+    this.setState({
+      moreInfo: false,
+      home: true
+    });
+  }
 
-	attendTogglerMoney(e) {
-		e.preventDefault();
-		this.setState({
-			moreInfo: false,
-			attendMoney: true
-		});
-	}
+  attendTogglerMoney(e) {
+    e.preventDefault();
+    this.setState({
+      moreInfo: false,
+      attendMoney: true
+    });
+  }
 
-	render() {
-		const container = {
-			margin: '50px auto 0',
-			width: '700px',
-			boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'
-		};
+  render() {
+    const container = {
+      margin: "50px auto 0",
+      width: "700px",
+      boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"
+    };
 
-		const cardMedia = {
-			borderRadius: '2px',
-			boxShadow: '0 2px 8px rgba(0, 0, 0, .12)',
-			height: '125px',
-			marginBottom: '25px',
-			transition: 'all 300ms ease-out',
-			width: '100%'
-		};
+    const cardMedia = {
+      borderRadius: "2px",
+      boxShadow: "0 2px 8px rgba(0, 0, 0, .12)",
+      height: "125px",
+      marginBottom: "25px",
+      transition: "all 300ms ease-out",
+      width: "100%"
+    };
 
-		const ps = {
-			padding: '2px 16px'
-		};
+    const ps = {
+      padding: "2px 16px"
+    };
 
-		return (
-			<div>
-				{this.state.moreInfo ? (
-					<div style={container}>
-						<img src={this.props.eventDescription[this.props.index].imgUrl[0]} style={{ width: '100%' }} />
-						<h3>Date: {this.props.eventDescription[this.props.index].date}</h3>
-						<h3>Name: {this.props.eventDescription[this.props.index].eventName}</h3>
-						<p>Description: {this.props.eventDescription[this.props.index].description}</p>
-						<video
-							width="700"
-							height="480"
-							src={this.props.eventDescription[this.props.index].videos[0]}
-							controls
-						/>
+    return (
+      <div>
+        {this.state.moreInfo ? (
+          <div style={container}>
+            <img
+              src={this.props.eventDescription[this.props.index].imgUrl[0]}
+              style={{ width: "100%" }}
+            />
+            <h3>Date: {this.props.eventDescription[this.props.index].date}</h3>
+            <h3>
+              Name: {this.props.eventDescription[this.props.index].eventName}
+            </h3>
+            <p>
+              Description:{" "}
+              {this.props.eventDescription[this.props.index].description}
+            </p>
+            <video
+              width="700"
+              height="480"
+              src={this.props.eventDescription[this.props.index].videos[0]}
+              controls
+            />
 
-<<<<<<< HEAD
-						<p>{this.props.eventDescription[this.props.index].category}</p>
-						<p>{this.props.eventDescription[this.props.index].description}</p>
-						<button type="submit" onClick={this.updateState.bind(this)} style={cardMedia}>
-							back
-						</button>
-						{this.props.eventDescription[this.props.index].cost === 'FREE' ? (
-							<button type="submit" onClick={this.attendToggler.bind(this)} style={cardMedia}>
-								FREE
-							</button>
-						) : (
-							<button type="submit" onClick={this.attendTogglerMoney.bind(this)} style={cardMedia}>
-								Attend
-							</button>
-						)}
-					</div>
-				) : this.state.attendMoney ? (
-					<Attend
-						eventId={this.props.eventDescription[this.props.index].id}
-						userId="5e148bf8fc13ae0c40000000"
-						events={this.props.eventDescription}
-					/>
-				) : (
-					<EventsList events={this.props.eventDescription} />
-				)}
-			</div>
-		);
-	}
-=======
             <p>{this.props.eventDescription[this.props.index].category}</p>
             <p>{this.props.eventDescription[this.props.index].description}</p>
-            <button type="submit" onClick={this.updateState.bind(this)} style={cardMedia}>
-                back
-              </button>
+            <button
+              type="submit"
+              onClick={this.updateState.bind(this)}
+              style={cardMedia}
+            >
+              back
+            </button>
             {this.props.eventDescription[this.props.index].cost === "FREE" ? (
-              <button type="submit" onClick={this.attendToggler.bind(this)} style={cardMedia}>
+              <button
+                type="submit"
+                onClick={this.attendToggler.bind(this)}
+                style={cardMedia}
+              >
                 FREE
               </button>
             ) : (
               <button
                 type="submit"
-                onClick={this.attendTogglerMoney.bind(this)} style={cardMedia}
+                onClick={this.attendTogglerMoney.bind(this)}
+                style={cardMedia}
               >
                 Attend
               </button>
@@ -141,17 +129,15 @@ class MoreInfo extends Component {
         ) : this.state.attendMoney ? (
           <Attend
             eventId={this.props.eventDescription[this.props.index].id}
-            userId="5e148bf8fc13ae0c40000006"
+            userId="5e148bf8fc13ae0c40000000"
             events={this.props.eventDescription}
           />
-        )
-        :
-        <EventsList events={this.props.eventDescription} />
-      }
+        ) : (
+          <EventsList events={this.props.eventDescription} />
+        )}
       </div>
     );
   }
->>>>>>> f87c584f37b4f95b7e3abb134484c1743af912de
 }
 
 export default MoreInfo;
