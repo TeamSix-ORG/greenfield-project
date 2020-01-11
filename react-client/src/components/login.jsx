bb6a4d7575ad523f83741ea5459acab094713368
SofianSaleh <sofiane.saleh11@gmail.com>
2020-01-11 11:18:43 +0100

    stylin login and sign up user done

--- react-client/src/components/login.jsx
+++ react-client/src/components/login.jsx
@@ -1,101 +1,139 @@
-import React from 'react';
-import $ from 'jquery';
-import { Link } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import $ from "jquery";
+import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 class Login extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			username: '',
-			email: '',
-			password: '',
-			type: 'user',
-			authentified: false
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	handleChange(e) {
-		this.setState({ [e.target.name]: e.target.value });
-	}
-	handleSubmit(event) {
-		event.preventDefault();
-		$.ajax({
-			type: 'POST',
-			url: '/api/login',
-			dataType: 'text',
-			data: this.state,
-			contentType: 'application/x-www-form-urlencoded',
-			success: (data) => {
-				console.log(data);
-				console.log(this.state);
-				this.setState({
-					authentified: true
-				});
-				console.log(this.state);
-				localStorage.setItem('user', JSON.stringify(data));
-			},
-			error: (err) => {
-				if (err) {
-					console.log(err);
-				}
-			}
-		});
-	}
-	render() {
-		if (this.state.authentified) {
-			return (
-				<Redirect
-					to={{
-						pathname: '/userdashboard'
-					}}
-				/>
-			);
-		}
-		const { username, email, password } = this.state;
-		return (
-			<div>
-				<h1>create a user account</h1>
-				<form onSubmit={this.handleSubmit}>
-					<label>
-						Username:
-						<input
-							type="text"
-							placeholder="Choose username"
-							name="username"
-							value={username}
-							onChange={this.handleChange}
-							required
-						/>
-					</label>
-					<label>
-						Email:
-						<input
-							type="text"
-							placeholder="enter your email"
-							name="email"
-							value={email}
-							onChange={this.handleChange}
-							required
-						/>
-					</label>
-					<label>
-						Password:
-						<input
-							type="password"
-							placeholder="enter your password"
-							name="password"
-							value={password}
-							onChange={this.handleChange}
-							required
-						/>
-					</label>
-					<button type="submit">Submit</button>
-				</form>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      type: "user",
+      authentified: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/api/login",
+      dataType: "text",
+      data: this.state,
+      contentType: "application/x-www-form-urlencoded",
+      success: data => {
+        console.log(data);
+        console.log(this.state);
+        this.setState({
+          authentified: true
+        });
+        console.log(this.state);
+        localStorage.setItem("user", JSON.stringify(data));
+      },
+      error: err => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    });
+  }
+  render() {
+    if (this.state.authentified) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/userdashboard"
+          }}
+        />
+      );
+    }
+    const { username, email, password } = this.state;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5">
+              <div className="card-body">
+                <h5 className="card-title text-center">Sign In</h5>
+                <form className="form-signin" onSubmit={this.handleSubmit}>
+                  <div className="form-label-group">
+                    <input
+                      type="text"
+                      id="inputEmail"
+                      name="username"
+                      value={username}
+                      onChange={this.handleChange}
+                      className="form-control"
+                      placeholder="Username"
+                      required
+                      autoFocus
+                    />
+                    <label htmlFor="inputEmail">Username</label>
+                  </div>
+
+                  <div className="form-label-group">
+                    <input
+                      type="email"
+                      id="inputEmail"
+                      name="email"
+                      value={email}
+                      onChange={this.handleChange}
+                      className="form-control"
+                      placeholder="Email address"
+                      required
+                      autoFocus
+                    />
+                    <label htmlFor="inputEmail">Email address</label>
+                  </div>
+
+                  <div className="form-label-group">
+                    <input
+                      type="password"
+                      id="inputPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      name="password"
+                      value={password}
+                      onChange={this.handleChange}
+                      required
+                    />
+                    <label htmlFor="inputPassword">Password</label>
+                  </div>
+
+                  <div className="custom-control custom-checkbox mb-3">
+                    <input
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="customCheck1"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customCheck1"
+                    >
+                      Remember password
+                    </label>
+                  </div>
+                  <button
+                    className="btn btn-lg btn-primary btn-block text-uppercase"
+                    type="submit"
+                  >
+                    Sign in
+                  </button>
+                  <hr className="my-4" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
